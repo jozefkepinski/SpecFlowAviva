95ab90af6888c77ee723407f507de49625d886eb
jozefkepinski <jozefkepinski@hotmail.com>
2024-10-22 22:37:05 +0200

    Dodaj pliki projektów.

---
+++ Features/AvivaMainPageCheck.feature
@@ -0,0 +1,9 @@
+﻿Feature: AvivaMainPageCheck
+
+A short summary of the feature is to go to Aviva web page and validate it.
+
+@TestAvivaWebPage
+Scenario: Check Aviva web page
+	Given Open web browser
+	When Navigated to the Aviva home 
+    Then The title should be "Aviva corporate website - Aviva plc" 
---
+++ StepDefinitions/AvivaMainPageStepDefinition.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+
+namespace SpecFlowAviva.StepDefinitions
+{
+    [Binding]
+    public sealed class AvivaMainPageCheckStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        private IWebDriver _driver;
+        [Given(@"Open web browser")]
+        public void GivenOpenWebBrowser()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [When(@"Navigated to the Aviva home")]
+        public void WhenNavigatedToTheAvivaHome()
+        {
+            _driver.Url = "https://www.aviva.com/";
+            _driver.Navigate();
+            var accept_all_coockies_button = _driver.FindElement(By.XPath("//button[@id='onetrust-accept-btn-handler']"));
+            accept_all_coockies_button.Click();
+        }
+
+        [Then(@"The title should be ""([^""]*)""")]
+        public void ThenTheTitleShouldBe(string expectedTitle)
+        {
+            var actualTitle = _driver.Title;
+            Assert.AreEqual(expectedTitle, actualTitle);
+            _driver.Quit();
+        }
+
+    }
+}
