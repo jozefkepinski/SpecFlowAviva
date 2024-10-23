8ad8f8f68debbf9039b4bd2a1e1fbf53044ceb5e
jozefkepinski <jozefkepinski@hotmail.com>
2024-10-23 14:39:38 +0200

    Added ExtentSparkReporter

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -1,17 +1,47 @@
-﻿using BoDi;
+﻿using AventStack.ExtentReports.Gherkin.Model;
+using BoDi;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SpecFlowAviva.Utility;
 
 namespace SpecFlowAviva.Hooks
 {
     [Binding]
-    public sealed class Hooks
+    public sealed class Hooks : ExtentReport
     {
         private readonly IObjectContainer _container;
         public Hooks(IObjectContainer container)
         {
             _container = container;
         }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            Console.WriteLine("Running before test run...");
+            ExtentReportInit();
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            Console.WriteLine("Running after test run...");
+            ExtetentReportTearDown();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine("Running before feature...");
+            _feature = _extentReports.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [AfterFeature]
+        public static void AfterFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine("Running after feature...");
+        }
+
 
         [BeforeScenario("@TestAvivaWebPage")]
         public void BeforeScenarioWithTag()
@@ -20,12 +50,14 @@ namespace SpecFlowAviva.Hooks
         }
 
         [BeforeScenario(Order = 1)]
-        public void FirstBeforeScenario()
+        public void FirstBeforeScenario(ScenarioContext scenarioContext)
         {
             IWebDriver _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
 
             _container.RegisterInstanceAs<IWebDriver>(_driver);
+
+            _scenario = _feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
         }
 
         [AfterScenario]
@@ -36,6 +68,47 @@ namespace SpecFlowAviva.Hooks
             if (driver != null)
             {
                 driver.Quit();
+            }
+        }
+
+        [AfterStep]
+        public void AfterStep(ScenarioContext scenarioContext)
+        {
+            Console.WriteLine("Running after scenario...");
+            string stepType = scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            string stepName = scenarioContext.StepContext.StepInfo.Text;
+
+            //When scenario passed
+            if (scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName);
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName);
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName);
+                }
+            }
+            //When scenario fails
+            if (scenarioContext.TestError != null)
+            {
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName).Fail(scenarioContext.TestError.Message);
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName).Fail(scenarioContext.TestError.Message);
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName).Fail(scenarioContext.TestError.Message);
+                }
             }
         }
     }
---
+++ Utility/ExtentReport.cs
@@ -0,0 +1,38 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Config;
+
+namespace SpecFlowAviva.Utility
+{
+    public class ExtentReport
+    {
+        public static ExtentReports _extentReports;
+        public static ExtentTest _feature;
+        public static ExtentTest _scenario;
+        public static ExtentSparkReporter _sparkReporter;
+        public static String dir = AppDomain.CurrentDomain.BaseDirectory;
+        public static String testResultPath = dir.Replace("bin\\Debug\\net6.0", "TestResults");
+
+        public static void ExtentReportInit()
+        {
+            _sparkReporter = new ExtentSparkReporter(testResultPath + "\\index.html");
+            _sparkReporter.Config.ReportName = "Automation Aviva Status Report";
+            _sparkReporter.Config.DocumentTitle = "Status Report";
+            _sparkReporter.Config.Theme = Theme.Standard;
+            
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(_sparkReporter);
+            _extentReports.AddSystemInfo("Application", "Aviva");
+            _extentReports.AddSystemInfo("Browser", "Chrome");
+            _extentReports.AddSystemInfo("OS", "Windows");
+        }
+
+
+        public static void ExtetentReportTearDown() 
+        {
+            _extentReports.Flush();
+        }
+
+
+    }
+}
